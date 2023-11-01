dc0d91f5d8fe4154b2b473cecdb434ba4b60395c
nikola.ninov <momzzze@abv.bg>
2023-11-01 14:27:36 +0200

    add Companion creation form, add category filter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental:{
+        serverActions:true
+    },
+    images:{
+        domains:['localhost','res.cloudinary.com']
+    }
+}
 
 module.exports = nextConfig
